7ac19aafc8506e01c44cefacaed0a50e2d93f4b7
ShayBenyo11 <84469682+ShayBenyo11@users.noreply.github.com>
2021-11-10 13:37:24 +0200

    Update drives.go

--- cmd/kubectl-direct_csi/drives.go
+++ cmd/kubectl-direct_csi/drives.go
@@ -19,8 +19,8 @@
 package main
 
 import (
-	directcsi "github.com/minio/direct-csi/pkg/apis/direct.csi.min.io/v1beta3"
-	"github.com/minio/direct-csi/pkg/utils"
+	directcsi "github.com/ShayBenyo11/directcsi-minio/pkg/apis/direct.csi.min.io/v1beta3"
+	"github.com/ShayBenyo11/directcsi-minio/pkg/utils"
 	"github.com/spf13/cobra"
 )
 
